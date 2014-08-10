f9584f460d4d872a9a48f5dd5573432ecb412a0e
Colin Scott <colin@abstractcode.com>
2014-08-10 10:17:56 +0800

    Test web role

---
+++ TestWebRole/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWebRole.Startup))]
+namespace TestWebRole
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
