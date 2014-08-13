dd710c7e5bf9e8511380077dd308fdc022b00c77
Colin Scott <colin@abstractcode.com>
2014-08-13 11:53:39 +0800

    Add factory method for creation cloud block blobs

---
+++ LightBlue/AzureBlockBlobFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using LightBlue.Hosted;
+using LightBlue.Standalone;
+
+namespace LightBlue
+{
+    public static class AzureBlockBlobFactory
+    {
+        public static IAzureBlockBlob Build(Uri blobUri)
+        {
+            return blobUri.Scheme == "file"
+                ? (IAzureBlockBlob)new StandaloneAzureBlockBlob(blobUri)
+                : new HostedAzureBlockBlob(blobUri);
+        }
+    }
+}
