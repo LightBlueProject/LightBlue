d1123e1d16279cb0c03780865a253ad2f598dfd0
Colin Scott <colin@abstractcode.com>
2014-08-19 20:00:30 +0800

    Fix build script for systems without VS2012

--- Scripts/BuildAndPackage.cmd
+++ Scripts/BuildAndPackage.cmd
@@ -1,2 +1,2 @@
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%~dp0..\LightBlue.sln" /target:Clean,Build /p:Configuration=Release /maxcpucount
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "%~dp0..\LightBlue.sln" /target:Clean,Build /p:Configuration=Release;VisualStudioVersion=12.0 /maxcpucount
 powershell.exe %~dp0PackageNuget.ps1
