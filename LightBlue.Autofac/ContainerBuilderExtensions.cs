a20a94f41d1379a5a70f5afbaeb6f7ded8f4dcb6
Colin Scott <colin@abstractcode.com>
2014-08-23 22:29:42 +0800

    Register common elements after specific so that the type scan doesn't wipe them out.

--- LightBlue.Autofac/ContainerBuilderExtensions.cs
+++ LightBlue.Autofac/ContainerBuilderExtensions.cs
@@ -8,17 +8,17 @@ namespace LightBlue.Autofac
     {
         public static void RegisterLightBlueModules(this ContainerBuilder builder)
         {
-            builder.RegisterModule<LightBlueCommonModule>();
-
             if (LightBlueContext.AzureEnvironment == AzureEnvironment.LightBlue)
             {
                 builder.RegisterModule<LightBlueStandaloneModule>();
+                builder.RegisterModule<LightBlueCommonModule>();
                 return;
             }
 
             if (LightBlueContext.AzureEnvironment == AzureEnvironment.ActualAzure || LightBlueContext.AzureEnvironment == AzureEnvironment.Emulator)
             {
                 builder.RegisterModule<LightBlueHostedModule>();
+                builder.RegisterModule<LightBlueCommonModule>();
                 return;
             }
 
