cdf55dee38093317d0a27abe7b51ded97aefde13
Colin Scott <colin.scott@cashconverters.com>
2014-08-26 13:58:10 +0800

    Update to version 0.1.9

--- LightBlue/Properties/AssemblyInfo.cs
+++ LightBlue/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("743b834d-6da7-4cab-9199-4f80f9adf6a8")]
 
-[assembly: AssemblyVersion("0.1.8.0")]
-[assembly: AssemblyFileVersion("0.1.8.0")]
-[assembly: AssemblyInformationalVersion("0.1.8.0")]
+[assembly: AssemblyVersion("0.1.9.0")]
+[assembly: AssemblyFileVersion("0.1.9.0")]
+[assembly: AssemblyInformationalVersion("0.1.9.0")]
 
 [assembly: CLSCompliant(false)]
