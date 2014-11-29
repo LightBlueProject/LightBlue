902ee8e934c186ca219162138657303d4b592af2
Colin Scott <colin@abstractcode.com>
2014-11-29 11:45:15 +0800

    Update to version 1.1.15

--- TestWebRole/Properties/AssemblyInfo.cs
+++ TestWebRole/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("cdb075fb-bd5f-49c1-9e33-545a43b36abd")]
 
-[assembly: AssemblyVersion("1.1.14.0")]
-[assembly: AssemblyFileVersion("1.1.14.0")]
-[assembly: AssemblyInformationalVersion("1.1.14.0")]
+[assembly: AssemblyVersion("1.1.15.0")]
+[assembly: AssemblyFileVersion("1.1.15.0")]
+[assembly: AssemblyInformationalVersion("1.1.15.0")]
 
 [assembly: CLSCompliant(false)]
