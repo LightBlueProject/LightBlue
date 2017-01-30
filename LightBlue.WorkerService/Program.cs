83650d86269da4ef12733e7f80d930b3e2c61f2a
Drewan <drewan.obrien@cashconverters.com>
2017-01-30 09:39:35 +0800

    move classes to own file

--- LightBlue.WorkerService/Program.cs
+++ LightBlue.WorkerService/Program.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Configuration;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using LightBlue.Setup;
-using Microsoft.WindowsAzure.ServiceRuntime;
+﻿using System.Diagnostics;
 using Topshelf;
 using Topshelf.Logging;
 
@@ -20,7 +12,7 @@ namespace LightBlue.WorkerService
             {
                 Trace.Listeners.Add(new TopshelfConsoleTraceListener());
 
-                var settings = WorkerHost.Settings.Load();
+                var settings = Settings.Load();
 
                 x.Service<WorkerHost>(service =>
                 {
@@ -40,112 +32,6 @@ namespace LightBlue.WorkerService
                     rc.RestartService(1);
                 });
             });
-        }
-
-        public class WorkerHost
-        {
-            private readonly Settings _settings;
-            private RoleEntryPoint _role;
-            private HostControl _hostControl;
-
-            public WorkerHost(Settings settings)
-            {
-                _settings = settings;
-            }
-
-            public bool Start(HostControl hc)
-            {
-                _hostControl = hc;
-
-                var hostDirectory = LightBlueConfiguration.SetAsWindowsHost(_settings.ServiceTitle,
-                    _settings.Cscfg,
-                    _settings.Csdef,
-                    _settings.RoleName);
-
-                Trace.TraceInformation("Worker host service LightBlue context created at directory {0}", hostDirectory);
-
-                var assemblyPath = Path.IsPathRooted(_settings.Assembly)
-                    ? _settings.Assembly
-                    : Path.Combine(Environment.CurrentDirectory, _settings.Assembly);
-                var entryPoint = Assembly.LoadFrom(assemblyPath)
-                    .GetTypes()
-                    .Single(t => typeof(RoleEntryPoint).IsAssignableFrom(t));
-                _role = (RoleEntryPoint)Activator.CreateInstance(entryPoint);
-
-                Trace.TraceInformation("Worker host service role entry point {0} located at {1}", entryPoint.FullName, assemblyPath);
-
-                if (!_role.OnStart())
-                {
-                    Trace.TraceError("Worker host service role entry point {0} start failed", entryPoint.FullName);
-                    _hostControl.Restart();
-                    return true;
-                }
-
-                Task.Run(() =>
-                {
-                    try
-                    {
-                        _role.Run();
-                    }
-                    catch (Exception ex)
-                    {
-                        Trace.TraceError("Worker host service errored with exception: {0}", ex);
-                        _hostControl.Restart();
-                    }
-                });
-
-                Trace.TraceInformation("Worker host service role entry point {0} running", entryPoint.FullName);
-
-                return true;
-            }
-
-            public void Stop()
-            {
-                _role.OnStop();
-                Trace.TraceInformation("Worker host service restarting");
-            }
-
-            public class Settings
-            {
-                public string Assembly { get; set; }
-                public string RoleName { get; set; }
-                public string ServiceTitle { get; set; }
-                public string Cscfg { get; set; }
-                public string Csdef { get; set; }
-
-                public static Settings Load()
-                {
-                    var settings = new Settings
-                    {
-                        Assembly = ConfigurationManager.AppSettings["Assembly"],
-                        RoleName = ConfigurationManager.AppSettings["RoleName"],
-                        ServiceTitle = ConfigurationManager.AppSettings["ServiceTitle"],
-                        Cscfg = ConfigurationManager.AppSettings["Cscfg"],
-                        Csdef = ConfigurationManager.AppSettings["Csdef"]
-                    };
-
-                    if (string.IsNullOrWhiteSpace(settings.Assembly))
-                        throw new InvalidOperationException("Host requires an assembly to run.");
-
-                    if (string.IsNullOrWhiteSpace(settings.RoleName))
-                        throw new InvalidOperationException("Role Name must be specified.");
-
-                    if (string.IsNullOrWhiteSpace(settings.Cscfg))
-                        throw new InvalidOperationException("Cscfg path must be specified.");
-
-                    if (string.IsNullOrWhiteSpace(settings.Csdef))
-                        throw new InvalidOperationException("Csdef path must be specified.");
-
-                    var absoluteAssemblyPath = Path.IsPathRooted(settings.Assembly)
-                        ? settings.Assembly
-                        : Path.Combine(Environment.CurrentDirectory, settings.Assembly);
-
-                    if (!File.Exists(absoluteAssemblyPath))
-                        throw new InvalidOperationException("The specified site assembly cannot be found.");
-
-                    return settings;
-                }
-            }
         }
     }
 }
---
+++ LightBlue.WorkerService/WorkerHost.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using LightBlue.Setup;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Topshelf;
+
+namespace LightBlue.WorkerService
+{
+    internal class WorkerHost
+    {
+        private readonly Settings _settings;
+        private RoleEntryPoint _role;
+        private HostControl _hostControl;
+
+        public WorkerHost(Settings settings)
+        {
+            _settings = settings;
+        }
+
+        public bool Start(HostControl hc)
+        {
+            _hostControl = hc;
+
+            var hostDirectory = LightBlueConfiguration.SetAsWindowsHost(_settings.ServiceTitle,
+                _settings.Cscfg,
+                _settings.Csdef,
+                _settings.RoleName);
+
+            Trace.TraceInformation("Worker host service LightBlue context created at directory {0}", hostDirectory);
+
+            var assemblyPath = Path.IsPathRooted(_settings.Assembly)
+                ? _settings.Assembly
+                : Path.Combine(Environment.CurrentDirectory, _settings.Assembly);
+            var entryPoint = Assembly.LoadFrom(assemblyPath)
+                .GetTypes()
+                .Single(t => typeof(RoleEntryPoint).IsAssignableFrom(t));
+            _role = (RoleEntryPoint)Activator.CreateInstance(entryPoint);
+
+            Trace.TraceInformation("Worker host service role entry point {0} located at {1}", entryPoint.FullName, assemblyPath);
+
+            if (!_role.OnStart())
+            {
+                Trace.TraceError("Worker host service role entry point {0} start failed", entryPoint.FullName);
+                _hostControl.Restart();
+                return true;
+            }
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    _role.Run();
+                }
+                catch (Exception ex)
+                {
+                    Trace.TraceError("Worker host service errored with exception: {0}", ex);
+                    _hostControl.Restart();
+                }
+            });
+
+            Trace.TraceInformation("Worker host service role entry point {0} running", entryPoint.FullName);
+
+            return true;
+        }
+
+        public void Stop()
+        {
+            _role.OnStop();
+            Trace.TraceInformation("Worker host service restarting");
+        }
+    }
+}
