2828344e42aad4d547c42bc255da442e7f24d9a6
Eric Winkler <eric@winkler.net.au>
2022-10-18 16:19:03 +0800

    New csproj format - LightBlue.Tests

--- LightBlue.Tests/Properties/AssemblyInfo.cs
+++ LightBlue.Tests/Properties/AssemblyInfo.cs
@@ -2,21 +2,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("LightBlue.Tests")]
-[assembly: AssemblyDescription("Tests for the LightBlue Azure development platform.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Colin David Scott")]
-[assembly: AssemblyProduct("LightBlue.Tests")]
-[assembly: AssemblyCopyright("Copyright © 2014-2015 LightBlue contributors")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
 [assembly: Guid("0181ae49-63e8-4b10-8123-3711c779aae1")]
-
-[assembly: AssemblyVersion("1.1.21.0")]
-[assembly: AssemblyFileVersion("1.1.21.0")]
-[assembly: AssemblyInformationalVersion("1.1.21.0")]
 
 [assembly: CLSCompliant(false)]
