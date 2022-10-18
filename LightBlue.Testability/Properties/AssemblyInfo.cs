5e09ca668967f2faecb213ea59a9fc41564b9699
Eric Winkler <eric@winkler.net.au>
2022-10-18 16:14:24 +0800

    New csproj format - LightBlue.Testability

--- LightBlue.Testability/Properties/AssemblyInfo.cs
+++ LightBlue.Testability/Properties/AssemblyInfo.cs
@@ -2,21 +2,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("LightBlue.Testability")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Colin David Scott")]
-[assembly: AssemblyProduct("LightBlue.Testability")]
-[assembly: AssemblyCopyright("Copyright © LightBlue contributors 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
 [assembly: Guid("da95e3d0-a72f-42de-b7f9-debca84112de")]
-
-[assembly: AssemblyVersion("1.1.21.0")]
-[assembly: AssemblyFileVersion("1.1.21.0")]
-[assembly: AssemblyInformationalVersion("1.1.21.0")]
 
 [assembly: CLSCompliant(false)]
