fbcc22cd8b4b3c6805ec88a07c05797d9e2bacf3
Eric Winkler <eric@winkler.net.au>
2022-10-18 17:13:02 +0800

    New csproj format - LightBlue.Windsor

--- LightBlue.Windsor/Properties/AssemblyInfo.cs
+++ LightBlue.Windsor/Properties/AssemblyInfo.cs
@@ -2,21 +2,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("LightBlue.Windsor")]
-[assembly: AssemblyDescription("Castle Windsor support for LightBlue")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Colin David Scott")]
-[assembly: AssemblyProduct("LightBlue.Windsor")]
-[assembly: AssemblyCopyright("Copyright © 2014-2015 LightBlue contributors")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
 [assembly: Guid("53e5662c-d44f-4ace-af32-52eeef15a96e")]
-
-[assembly: AssemblyVersion("1.1.21.0")]
-[assembly: AssemblyFileVersion("1.1.21.0")]
-[assembly: AssemblyInformationalVersion("1.1.21.0")]
 
 [assembly: CLSCompliant(false)]
