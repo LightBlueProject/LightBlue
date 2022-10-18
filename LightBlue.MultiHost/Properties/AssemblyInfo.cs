b1f7bf71c5bc2f4961bfab739ee57c1308a3e5f6
Eric Winkler <eric@winkler.net.au>
2022-10-18 09:47:53 +0800

    New csproj format - LightBlue.MultiHost

--- LightBlue.MultiHost/Properties/AssemblyInfo.cs
+++ LightBlue.MultiHost/Properties/AssemblyInfo.cs
@@ -3,15 +3,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyTitle("LightBlue.MultiHost")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Colin David Scott")]
-[assembly: AssemblyProduct("LightBlue.MultiHost")]
-[assembly: AssemblyCopyright("Copyright © LightBlue contributors 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 
 //In order to begin building localizable applications, set 
@@ -32,9 +23,5 @@ using System.Windows;
     //(used if a resource is not found in the page, 
     // app, or any theme specific resource dictionaries)
 )]
-
-[assembly: AssemblyVersion("1.1.21.0")]
-[assembly: AssemblyFileVersion("1.1.21.0")]
-[assembly: AssemblyInformationalVersion("1.1.21.0")]
 
 [assembly: CLSCompliant(false)]
