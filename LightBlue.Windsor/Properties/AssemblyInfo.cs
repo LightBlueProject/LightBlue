f0b24a54cc2331509e55a1dbd988a15d58f88828
Eric Winkler <eric@winkler.net.au>
2022-10-20 15:09:41 +0800

    Remove LightBlue.Windsor

--- LightBlue.Windsor/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("53e5662c-d44f-4ace-af32-52eeef15a96e")]
-
-[assembly: CLSCompliant(false)]
