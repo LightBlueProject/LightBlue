210fb921553787ef7795cf81dfa446b530f6d2f2
Eric Winkler <eric@winkler.net.au>
2024-02-02 16:34:49 +0800

    Disabling test concurrency

--- LightBlue.Tests/Properties/AssemblyInfo.cs
+++ LightBlue.Tests/Properties/AssemblyInfo.cs
@@ -1,9 +1,12 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
+using Xunit;
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("0181ae49-63e8-4b10-8123-3711c779aae1")]
 
 [assembly: CLSCompliant(false)]
+
+// Many of our tests reference shared state (StandaloneEnvironment.LightBlueDataDirectory) and cannot be run concurrently
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
